1bd83ec6251c5c6ee6d47970212c2b6a54879b6d
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-08-02 11:32:15 +0200

    setup: lint files on commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,6 @@
 module.exports = {
   root: true,
   extends: ['@linusborg'],
-  // extends: [
-  //   'plugin:vue/vue3-essential',
-  //   '@vue/typescript',
-  //   'eslint:recommended',
-  //   'plugin:@typescript-eslint/eslint-recommended',
-  //   'prettier/@typescript-eslint',
-  //   'plugin:prettier/recommended',
-  // ],
   overrides: [
     {
       files: ['*.js'],
@@ -16,12 +8,5 @@ module.exports = {
         node: true,
       },
     },
-    //   {
-    //     files: ['*.spec.ts', '*.spec.js'],
-    //     env: {
-    //       jest: true,
-    //       browser: true,
-    //     },
-    //   },
   ],
 }
